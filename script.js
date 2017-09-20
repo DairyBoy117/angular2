b456473023c7ca26ae81123c513e0b2450c47b97
Austin Verburg <dairyboycomics@gmail.com>
2017-09-19 20:11:34 -0700

    Josh version

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function futurama() {
+/*function futurama() {
 	var x = document.getElementById("textinputid").value;
 	if (x == "Electric Mucus") {
 		document.getElementById("buttonid").disabled = false;
 	} else if (x == "pink") {
 		document.getElementById("buttonid").style.backgroundColor = "#FFC0CB";
 	}
-}
+}*/
+
+const button = window.document.getElementById("buttonid");
+button.disabled = true;
+
+window.document.getElementById("textinputid") //getting an html element
+	.addEventListener("input", 
+		function(event) {
+			const userInputText = event.target.value; //what the user types
+
+			button.style.backgroundColor = userInputText;
+
+			button.disabled = userInputText.toLowerCase() !== 'electric mucus';
+		}
+	);
