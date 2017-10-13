1d0078182e00980ef065b03f425fddbdc0247407
Austin Verburg <dairyboycomics@gmail.com>
2017-10-12 18:26:25 -0700

    logged out user sent back to home if navigating to room

--- gw-reserve/src/app/services/router-guard.ts
+++ gw-reserve/src/app/services/router-guard.ts
@@ -1,13 +1,19 @@
 ﻿import {Injectable} from '@angular/core';
-import {CanActivate} from '@angular/router';
+import {CanActivate, Router} from '@angular/router';
 import {LogInService} from './login.services';
 
 @Injectable()
 
 export class LogInRouterGuard implements CanActivate {
-    constructor(private login: LogInService) { }
+    constructor(private login: LogInService, private router: Router) { }
 
     canActivate() { //meant to return a boolean, if something calls this function, it will not allow that thing to continue if it gets a false
-        return Boolean( this.login.getLoggedInUser() ); //checks if a user is logged in
+        if (this.login.getLoggedInUser()) { //checks if a user is logged in
+            return true;
+        } else {
+            this.router.navigateByUrl('welcome');
+            return false;
+        }; 
+
     }
 }
