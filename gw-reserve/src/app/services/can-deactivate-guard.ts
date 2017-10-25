6062392e1e7ac6be7f3a24a8854a741bc77f2a68
Austin Verburg <dairyboycomics@gmail.com>
2017-10-24 20:01:42 -0700

    added children to rooms

--- gw-reserve/src/app/services/can-deactivate-guard.ts
+++ gw-reserve/src/app/services/can-deactivate-guard.ts
@@ -8,13 +8,10 @@ export interface ICanDeactivate { //creates interface that tells it to expect a 
 @Injectable()
 export class CanDeactivateService implements CanDeactivate<ICanDeactivate> {
     canDeactivate(component: ICanDeactivate) {
-        console.log(component);
-
-
-        if (component.canDeactivate) { //gets a boolean to store in canDeactivate, only if canDeactivate function exists on the component
-            return component.canDeactivate();
-        }
-
-        return true;
+        const canComponentDeactivate = component.canDeactivate ? component.canDeactivate() : true;
+        
+        if (!canComponentDeactivate) alert("Action cancelled by CanDeactivateGuardService");
+        
+        return canComponentDeactivate;
     }
 }
